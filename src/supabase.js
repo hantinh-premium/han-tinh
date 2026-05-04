a24fa02e0c5b245c0569595ab3769ff9f754b8eb
hantinh-premium <chilicheelee@gmail.com>
2026-05-04 19:28:21 +0700

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -5,4 +5,3 @@ export const supabase = createClient(
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpwbWd1dWFqc3V3cnFvaW1qaW90Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4MDYzNDUsImV4cCI6MjA5MzM4MjM0NX0.lz45eUzwpVQvrDVdJSAxvCwWlwCZn7H0nRVAL9Oqkjk'
 )
  
-export const hasSupabase = true
